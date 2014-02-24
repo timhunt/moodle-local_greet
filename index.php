8ffd1996d40292f4598695e43f73b900156191f9
Tim Hunt <T.J.Hunt@open.ac.uk>
2014-02-24 15:49:34 +0100

    Fix coding style.

--- index.php
+++ index.php
@@ -1,4 +1,29 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * An example of a stand-alone Moodle script.
+ *
+ * Says Hello, {username}, or Hello {name} if the name is given in the URL.
+ *
+ * @package   local_greet
+ * @copyright 2014 Tim Hunt
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 require_once(dirname(__FILE__) . '/../../config.php');        // 1
 
 require_login();                                              // 2
